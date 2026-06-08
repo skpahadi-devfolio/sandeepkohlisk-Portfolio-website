1e0ac978d228b4a9f53c2339e466140ff3ca0d53
Sandeep <sandeepkohli180@gmail.com>
2026-06-08 12:25:11 +0530

    add address and modify contact form page

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
 import { contactMessage } from '../services/contactServices.js'
 import Fadeleft from '../Animations/Fadeleft.jsx'
+import Faderight from '../Animations/Faderight.jsx'
 
 const Contact = () => {
   const [form, setform] = useState({ name: "", email: "", message: "" })
@@ -53,9 +54,30 @@ const Contact = () => {
         theme="dark"
       />
 
-      <h1 className='my-16 text-3xl font-bold text-center  animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-transparent'>Contact Form</h1>
+      <div className='relative pb-20'>
+        <h1 className='my-8 text-3xl font-bold text-center  animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-transparent'>Contact</h1>
+
+        <Faderight>
+          <div className="md:left-16 md:absolute text-white md:my-20 my-12 md:top-12 flex gap-8 flex-col">
+       <div className="title-left text-center md:text-2xl">
+         <p>A simple title prefix</p>
+        <p className='md:text-4xl text-2xl'>SEND ME A MESSAGE</p>
+       </div>
+
+       <div className="address text-center md:text-xl">
+        <p>Getting in touch is easy!</p>
+        <p>RC-881, Vandana Enclave Khora, Shital Vihar</p>
+        <p> Noida 201301</p>
+        <p>+91 9582579119</p>
+        <p>sandeepkohli180@gmail.com</p>
+       </div>
+      </div>
+        </Faderight>
+      
+
+      <div className='md:absolute md:right-28 md:top-12'>
       <Fadeleft>
-      <form className='text-black bg-gradient-to-br from-[#040005] via-[#0a0011] to-[#11041b] w-[95vw] md:max-w-xl p-4 rounded-xl mx-auto my-10 text-xs md:text-base' action="">
+      <form className='text-black bg-gradient-to-br from-[#040005] via-[#0a0011] to-[#11041b] w-[95vw] md:max-w-xl md:p-10 p-6 rounded-xl mx-auto my-10 text-xs md:text-base shadow-lg shadow-blue-900' action="">
         <div className='flex flex-col justify-center items-center gap-5'>
           <input name='name' value={form.name} onChange={handleChange} className='bg-white p-4 rounded-md w-full max-w-lg' type="text" placeholder='Enter your Name' required />
           <input name='email' value={form.email} onChange={handleChange} className='bg-white p-4 rounded-md w-full max-w-lg' type="email" placeholder='Enter your Email' required />
@@ -64,6 +86,8 @@ const Contact = () => {
         </div>
       </form>
       </Fadeleft>
+      </div>
+      </div>
     </div>
   )
 }
