f470a2f71bf3180ec9d9618a262ce2ee94718c20
Sandeep <sandeepkohli180@gmail.com>
2026-09-22 20:36:52 +0530

    Remove Nodemailer Automation code because it was preventing problem

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,6 +1,5 @@
 //Controller Logic For Contact Page:-
 import Contact from "../models/contactModel.js";
-import transporter from "../service/contactForm.js";
 
 export const contactMessage = async (req, res) => {
     try {
@@ -12,19 +11,7 @@ export const contactMessage = async (req, res) => {
                 message: "Empty Field are required"
             })
         }
-        const messge = await Contact.create({ name, email, message })
-        await transporter.sendMail({
-            from: process.env.EMAIL_USER,
-            to: process.env.EMAIL_USER,
-            replyTo: email,
-            subject: `Portfolio Contact: ${name}`,
-            text: `Hello Sandeep Kohli,
-You Have Receive a New Message from your Portfolio,
-Someone Want's to connect with you,
-Name: ${name} 
-Email: ${email} 
-Message:${message}`,
-        })
+        const messge = await Contact.create({ name, email, message });
         return res.status(200).json({
             success: true,
             message: "Message sent SuccessFully!",
